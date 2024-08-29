ec3aafb84a7f65b1a61eb776594fd21d619ae005
https://github.com/Tanya794 <tatyana711994@gmail.com>
2024-08-28 19:15:28 -0700

    Admin permossions

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -2,9 +2,11 @@ from rest_framework import permissions
 
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
-    """Позволяет редактировать рецепт только автору."""
+    """Позволяет редактировать только автору/админу."""
 
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
+        if request.user.is_staff or (
+            request.method in permissions.SAFE_METHODS
+        ):
             return True
         return obj.author == request.user
